f9c1f8f108f1d3e3c90d73a56fdac66e1cd71256
saeedahmed78 <38943150+saeedahmed78@users.noreply.github.com>
2018-07-16 07:17:36 -0700

    sa

--- script.js
+++ script.js
@@ -4,28 +4,33 @@
 let users = [];
 let pass = [];
 function addUser() {
-    let user = document.querySelector("#fullName").value;
-    users.push(user);
-    localStorage.setItem("users", users.join(","));
+	let user = document.querySelector("#fullName").value;
+	users.push(user);
+	localStorage.setItem("users", users.join(","));
 
-    let password = document.querySelector("#password").value;
-    pass.push(password);
-    localStorage.setItem("pass", pass.join(","));
+	let password = document.querySelector("#password").value;
+	pass.push(password);
+	localStorage.setItem("pass", pass.join(","));
 
-    alert("Sucessfully signup");
-        window.location.replace("home.html");
 
+	if (user == "" || password == "") {
+		alert("please attempt all ");
+	}   else{
+		alert("sucessfull logged in")
+		window.location.replace("html/home.html");
+
+	}
 }
 
 
 // local storage
 
 function reloadUsers() {
-    if (localStorage.getItem("users") !== null) {
-        users = localStorage.getItem("users").split(",");
-        pass = localStorage.getItem("pass").split(",");
-    
-    }
+	if (localStorage.getItem("users") !== null) {
+		users = localStorage.getItem("users").split(",");
+		pass = localStorage.getItem("pass").split(",");
+		
+	}
 
 
 }
@@ -39,35 +44,44 @@ reloadUsers();
 function login(){
 
 	let fname = document.querySelector("#fname").value;
-	let pass = document.querySelector("#pass").value;
-    for ( i = 0; i < users.length; i++){
-	for ( i = 0; i < pass.length; i++){
+	let password = document.querySelector("#pass").value;
+	for ( i = 0; i < users.length; i++){
+	   for ( i = 0; i < pass.length; i++){
 
-	if(fname == users[i] ){
-        // if (pass == pass[i]) {
-	
-    	alert("loggedin");
-        window.location.replace("home.html");
+		   if(fname == users[i] && password == pass ){
+		
+		window.location.replace("home.html");
+
 // }
-        return	
-    }else{
-		alert("erorr");
-        return
+return  
+}else{
+  alert("erorr");
+  return
 }
-	}	
+}   
 }
 }
 
 
+
+
+// logout
+
+
+function chackLogIn(){
+	// localStorage.removeItem("currentUser");
+	window.location.replace("../index.html")
+}
 
 // post
 
-// let timeline = [];
+
 
 function post(){
 
-    let textarea = document.querySelector("textarea").value;
-    // timeline.push(textarea);
-    let post = document.querySelector("#posts");
-    post.innerHTML += "<div>"+textarea+"</div>";
+	let textarea = document.querySelector("textarea").value;
+	let post = document.querySelector("#posts");
+	post.innerHTML += "<div>"+textarea+"</div>";
+
+	document.querySelector(".para").style.display = "block"
 }
